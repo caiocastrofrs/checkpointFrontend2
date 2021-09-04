f4d800e513a09084d85e02a560543fa547da20b9
caiocastrofrs <caiocastrofrs@gmail.com>
2021-09-04 17:48:49 -0300

    subindo site com função adicionar/deletar/editar card finalizado///corrigir o problema do botão de editar card

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* document.getElementById('deletar').offsetParent.style.display = 'none'; */
 let divInserirCard = document.getElementById('pop-up');
 
 window.addEventListener('load',function() {
@@ -15,30 +14,99 @@ function abrirPopup() {
 function fecharPopup() {
     divInserirCard.style.display = "none";
 }
-//FUNÇÃO PARA RECUPERAR O CARD QUE TEM QUE SER EXCLUIDO E CHAMAR A FUNÇÃO DE EXCLUIR PASSANDO O CARD COMO PARAMETRO
-function excluirCard(imagem) {
+
+function excluirCard(card) {
     let cardContainer = document.getElementById('card-container');
-    var card = imagem.parentElement;
     cardContainer.removeChild(card);
 }
-function aparecerX(param){
-    param.children[0].style.display = "inline-block";
-}
-function removerX(param){
-    param.children[0].style.display = "none";
+function criarCard(caminhoImagem, tituloH2, descricaoP) {
+    let card = document.createElement('section');
+    card.classList.add('card');
+    card.setAttribute('onclick', "selecionarCard(this)");
+    let titulo = document.createElement('h2');
+    titulo.innerText = tituloH2
+    let img = document.createElement('img');
+    img.src = caminhoImagem;
+    img.alt = `poster do filme ${titulo}`
+    let descricao = document.createElement('p');
+    descricao.innerText = descricaoP
+
+    card.appendChild(img);
+    card.appendChild(titulo);
+    card.appendChild(descricao);
+
+    return card;
+
 }
 function inserirCard() {
     let img = document.getElementById('urlImg').value;
     let titulo = document.getElementById('titulo').value;
     let descricao = document.getElementById('descricao').value;
-    document.getElementById('card-container').innerHTML += 
-    `<section class="card" onmouseover="aparecerX(this)" onmouseout="removerX(this)">
-        <img src="./icons/cancel.png" id="excluir" name="btnExcluir" onclick="excluirCard(this)"/>
-        <img src="${img}"alt="poster do filme ${titulo}"/>
-        <h2>${titulo}</h2>
-        <p>${descricao}</p>
-    </section>`
+    let card = criarCard(img, titulo, descricao);
+    card.addEventListener('dblclick', function() {
+        this.classList.toggle('excluir-card');
+    })
+    document.getElementById('card-container').appendChild(card);
     fecharPopup(); 
 }
 
+function selecionarCard(card) {
+    if(card.classList.contains('excluir-card')) {
+        card.classList.remove('excluir-card');
+    } else if (card.classList.contains('editar-card')) { 
+        card.classList.remove('editar-card');
+    }else {
+        card.classList.toggle("editar-card");
+    }
+    
+}
+
+function checarCardsExcluir() {
+    let cards = document.querySelectorAll('.card');
+    for(card of cards) {
+        if(card.classList.contains("excluir-card")) {
+            excluirCard(card);
+        }
+    }
+}
+
+function qntdCardsEditar(arrayCards) {
+    let cardSelecionado;
+    let cont = 0;
+    for(card of arrayCards) {
+        if(card.classList.contains("editar-card")) {
+            cont++;
+            cardSelecionado = card;
+        } 
+    }
+    if(cont > 1) {
+        return false;
+    }else {
+        return cardSelecionado;
+    }   
+}
+function editarCard() {
+    let card = document.querySelector(".editar-card");
+    card.children[0].src = document.getElementById('urlImg').value;
+    card.children[1].innerText = document.getElementById('titulo').value;
+    card.children[2].innerText = document.getElementById('descricao').value;
+}
+function checarCardEditar() {
+    let cards = document.querySelectorAll('.card');
+    let cardOuFalso = qntdCardsEditar(cards);  
+    for(card of cards) {
+        if(card.classList.contains("editar-card")) {
+            if(!cardOuFalso) {
+                alert('Edite apenas um card por vez');
+            } else {
+                let botao = document.getElementById('btnEnviar');
+                botao.innerText = "Editar card";
+                botao.setAttribute('onclick',`editarCard()`);
+                abrirPopup();
+            }
+
+            
+        }
+    }
+}
 
