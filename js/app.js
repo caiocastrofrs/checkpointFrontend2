e95cef26699fdb9242c3f3c9e1344869412e0391
caiocastrofrs <caiocastrofrs@gmail.com>
2021-09-05 23:39:25 -0300

    subindo site finalizado e refatorado, menos bonito

--- js/app.js
+++ js/app.js
@@ -1,65 +1,85 @@
+//RECUPERANDO POP UP DE INSERIR CARD
 let divInserirCard = document.getElementById('pop-up');
-
 window.addEventListener('load',function() {
     divInserirCard.style.display = "none";
 })
-
+//PREVENINDO O RECARREGAMENTO DA PÁGINA AO DAR SUBMIT
 divInserirCard.addEventListener('submit', function(e){
     e.preventDefault();
 })
-
+//ABRIR FORM
 function abrirPopup() {
     divInserirCard.style.display = "flex";
 }
+//FECHAR FORM
 function fecharPopup() {
     divInserirCard.style.display = "none";
+    resetBotaoInserirCard();
 }
-
-function excluirCard(card) {
-    let cardContainer = document.getElementById('card-container');
-    cardContainer.removeChild(card);
-}
+//FUNÇÃO PARA CRIAR TODOS OS ELEMENTOS DO CARD
 function criarCard(caminhoImagem, tituloH2, descricaoP) {
     let card = document.createElement('section');
-    card.classList.add('card');
-    card.setAttribute('onclick', "selecionarCard(this)");
     let titulo = document.createElement('h2');
-    titulo.innerText = tituloH2
     let img = document.createElement('img');
-    img.src = caminhoImagem;
-    img.alt = `poster do filme ${titulo}`
     let descricao = document.createElement('p');
-    descricao.innerText = descricaoP
 
+    card.classList.add('card');
+    card.setAttribute('onclick', "addEditSelecao(this)");
+    card.setAttribute('draggable', 'true');
+    img.src = caminhoImagem;
+    img.alt = `poster do filme ${titulo}`;
+    titulo.innerText = tituloH2;
+    descricao.innerText = descricaoP;
     card.appendChild(img);
     card.appendChild(titulo);
     card.appendChild(descricao);
 
     return card;
-
 }
+//FUNÇÃO PARA INSERIR O CARD NA PÁGINA
 function inserirCard() {
     let img;
     let titulo;
     let descricao;
-    if((document.getElementById('urlImg').value.length > 0) && (document.getElementById('titulo').value.length > 0) && (document.getElementById('descricao').value.length > 0)) {
+    let card;
+    if(checarCamposVazios()) {
         img = document.getElementById('urlImg').value;
         titulo = document.getElementById('titulo').value;
         descricao = document.getElementById('descricao').value;
-        let card = criarCard(img, titulo, descricao);
+        card = criarCard(img, titulo, descricao);
+
         card.addEventListener('dblclick', function() {
             this.classList.toggle('excluir-card');
         })
+        card.style.animationName = "animacaoCriarCard";
         document.getElementById('card-container').appendChild(card);
         limparCampos();
         fecharPopup(); 
     } else {
-        document.getElementById('pop-up-alerta').style.animation = "popUpShowUp 1s both";
+        alert("Preencha todos os campos!");
     }
+}
+//FUNÇÃO PARA EXCLUIR O CARD
+function excluirCard(card) {
+    card.style.animationDuration = ".3s";
+    card.style.animationName = "animacaoExcluirCard";
+    setTimeout(function() {
+        document.getElementById('card-container').removeChild(card);
+    },290);
 
 }
-
-function selecionarCard(card) {
+//FUNÇÃO PARA CHECAR SE EXISTEM CAMPOS VAZIOS NO MOMENTO DA INSERÇÃO DOS DADOS DO CARD
+function checarCamposVazios() {
+    if((document.getElementById('urlImg').value.length > 0) && 
+    (document.getElementById('titulo').value.length > 0) && 
+    (document.getElementById('descricao').value.length > 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//FUNÇÃO PARA ADICIONAR A SELEÇÃO DE EDIÇÃO DO CARD
+function addEditSelecao(card) {
     if(card.classList.contains('excluir-card')) {
         card.classList.remove('excluir-card');
     } else if (card.classList.contains('editar-card')) { 
@@ -67,19 +87,23 @@ function selecionarCard(card) {
     }else {
         card.classList.toggle("editar-card");
     }
-    
 }
-
-function checarCardsExcluir() {
+//FUNÇÃO PARA RETORNAR UM ARRAY CONTENDO TODOS OS CARDS PRESENTES NA PÁGINA
+function getArrayCards() {
     let cards = document.querySelectorAll('.card');
+    return cards;
+}
+//FUNÇÃO PARA EXCLUIR TODO CARD QUE ESTIVER COM A SELEÇÃO DE EXCLUIR
+function checarCardsSelecaoExcluir() {
+    let cards = getArrayCards();
     for(card of cards) {
         if(card.classList.contains("excluir-card")) {
             excluirCard(card);
         }
     }
 }
-
-function qntdCardsEditar(arrayCards) {
+//FUNÇÃO PARA RETORNAR FALSO CASO TENHA MAIS DE UM CARD COM A SELEÇÃO DE EDIÇÃO OU RETORNA O CARD SELECIONADO
+function checkQntdCardsEditar(arrayCards) {
     let cardSelecionado;
     let cont = 0;
     for(card of arrayCards) {
@@ -94,10 +118,26 @@ function qntdCardsEditar(arrayCards) {
         return cardSelecionado;
     }   
 }
+//FUNÇÃO PARA MUDAR O BOTÃO DE INSERIR FILME PARA EDITAR CARD E SETAR A FUNÇÃO editarCard() NO BOTÃO
+function popUpEditar() {
+    let cards = getArrayCards();
+    let cardOuFalse = checkQntdCardsEditar(cards); 
+    console.log(cards);
+    if(cards.length == 0) {
+        alert("Não há cards!")
+    } else if(!cardOuFalse) {
+        alert('Edite apenas um card por vez');
+    }else {
+        let botao = document.getElementById('btnEnviar');
+        botao.innerText = "Editar card";
+        botao.setAttribute('onclick',`editarCard()`);
+        abrirPopup();
+    }
+}
+//FUNÇÃO PARA EDITAR AS INFORMAÇÕES DO CARD
 function editarCard() {
     let card = document.querySelector(".editar-card");
     if(document.getElementById('urlImg').value.length > 0) {
-
         card.children[0].src = document.getElementById('urlImg').value;
     }
     if(document.getElementById('titulo').value.length > 0) {
@@ -106,36 +146,23 @@ function editarCard() {
     if(document.getElementById('descricao').value.length > 0) {
         card.children[2].innerText = document.getElementById('descricao').value;
     }
+    resetBotaoInserirCard();
     fecharPopup();
     limparCampos();
-    resetBotaoInserirCard();
 }
-
-function limparCampos() {
-    document.getElementById('urlImg').value = "";
-    document.getElementById('titulo').value = "";
-    document.getElementById('descricao').value = "";
-}
+//FUNÇÃO PARA FORMATAR O BOTÃO DO FORM DE EDITAR CARD PARA INSERIR CARD
+//É NECESSARIO POIS ESTOU UTILIZANDO O MESMO FORMULÁRIO PARA CRIAR E EDITAR CARD
+//QUANDO O BOTÃO CRIAR CARD É CLICADO, O BOTÃO "INSERIR FILME" VEM COM A FUNÇÃO inserirCard();
+//QUANDO O BOTÃO EDITAR CARD É CLICADO, O BOTÃO "INSERIR FILME" VEM COM A FUNÇÃO editarCard();
 function resetBotaoInserirCard() {
     let botao = document.getElementById('btnEnviar');
     botao.innerText = "Inserir filme";
     botao.setAttribute('onclick',`inserirCard()`);
 }
-function checarCardEditar() {
-    let cards = document.querySelectorAll('.card');
-    let cardOuFalso = qntdCardsEditar(cards);  
-    for(card of cards) {
-        if(card.classList.contains("editar-card")) {
-            if(!cardOuFalso) {
-                alert('Edite apenas um card por vez');
-                break;
-            } else {
-                let botao = document.getElementById('btnEnviar');
-                botao.innerText = "Editar card";
-                botao.setAttribute('onclick',`editarCard()`);
-                abrirPopup();
-            }
-        }
-    }
+//FUNÇÃO PARA LIMPAR OS CAMPOS QUANDO O POP UP É FECHADO
+function limparCampos() {
+    document.getElementById('urlImg').value = "";
+    document.getElementById('titulo').value = "";
+    document.getElementById('descricao').value = "";
 }
 
