1152439ddc13adfcdb0e7322ee34126e6f83bdfa
caiocastrofrs <caiocastrofrs@gmail.com>
2021-09-14 02:10:37 -0300

    corrigindo animação do header

--- js/app.js
+++ js/app.js
@@ -172,4 +172,27 @@ function limparCampos() {
     document.getElementById('titulo').value = "";
     document.getElementById('descricao').value = "";
 }
-
+//FUNÇÃO PARA ATIVAR A ANIMAÇÃO DE EXPANDIR DO HEADER E A INSERÇÃO DOS DOIS LINKS 
+function expandirHeader() {
+    let divHeader = document.getElementById('header');
+    setTimeout(async function() {
+        divHeader.style.animationName = "animacaoExpandirHeader"; 
+        divHeader.style.width = "30%";
+         setTimeout(function() {
+            document.getElementById('equipe').style.opacity = "1";
+            document.getElementById('contato').style.opacity = "1";
+        },170)
+    },100);
+}
+//FUNÇÃO PARA ESCONDER OS ITENS DO HEADER E DESATIVAR A ANIMAÇÃO
+function esconderHeader() {
+    let divHeader = document.getElementById('header');
+    setTimeout(function () {
+        divHeader.style.animationName = "animacaoEsconderHeader";
+        divHeader.style.width = "200px";
+        setTimeout(function() {
+            document.getElementById('equipe').style.opacity = "0";
+            document.getElementById('contato').style.opacity = "0"; 
+        },150)
+    },100);
+}
